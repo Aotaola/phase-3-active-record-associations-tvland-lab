fc4d6af62367b4436cd64a94e7f80708e61474a9
Alejandro <alejelias@gmail.com>
2023-01-18 17:51:09 -0500

    commit tree 99b2aac2298b1875a9edde1011f650767c4fbe3b

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,10 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
   def change
+    create_table :characters do |t| 
+      t.string :name
+      t.references :actor
+      t.references :shows
+    end
     # Your code here
   end
 end
